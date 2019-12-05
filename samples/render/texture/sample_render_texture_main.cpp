acbb727c3897d73ed92686e208b7dec0c5776f91
myoukaku <myoukaku@hotmail.com>
2019-12-05 18:23:21 +0900

    Improve sample_render_texture

--- samples/render/texture/sample_render_texture_main.cpp
+++ samples/render/texture/sample_render_texture_main.cpp
@@ -8,6 +8,7 @@
 
 #include <bksge/window.hpp>
 #include <bksge/render.hpp>
+#include <bksge/math.hpp>
 #include <vector>
 #include <memory>
 #include <utility>
@@ -15,93 +16,180 @@
 namespace
 {
 
-static bksge::Shader const* GetGLSLShader(void)
+class Sprite
 {
-	char const* vs_source =
-		"attribute vec3 aPosition;					"
-		"attribute vec2 aTexCoord;					"
-		"varying   vec2 vTexCoord;					"
-		"											"
-		"void main()								"
-		"{											"
-		"	gl_Position = vec4(aPosition, 1.0);		"
-		"	vTexCoord   = aTexCoord;				"
-		"}											"
-	;
-
-	char const* fs_source =
-		"varying vec2 vTexCoord;					"
-		"uniform sampler2D uSampler;				"
-		"											"
-		"void main()								"
-		"{											"
-		"	gl_FragColor = texture2D(uSampler, vTexCoord);	"
-		"}											"
-	;
-
-	static bksge::Shader const shader
+private:
+	static bksge::Geometry const& GetGeometry(void)
 	{
-		bksge::ShaderType::kGLSL,
+		static bksge::Vertex<bksge::VPosition, bksge::VTexCoord> const vertices[] =
 		{
-			{ bksge::ShaderStage::kVertex,   vs_source },
-			{ bksge::ShaderStage::kFragment, fs_source },
-		}
-	};
+			{{{-0.5,  0.5, 0.0}}, {{0, 0}}},
+			{{{-0.5, -0.5, 0.0}}, {{0, 1}}},
+			{{{ 0.5,  0.5, 0.0}}, {{1, 0}}},
+			{{{ 0.5, -0.5, 0.0}}, {{1, 1}}},
+		};
+		static std::uint16_t const indices[] =
+		{
+			0, 1, 2,
+			2, 1, 3,
+		};
 
-	return &shader;
-}
+		static bksge::Geometry const geometry(
+			bksge::Primitive::kTriangles, vertices, indices);
 
-static bksge::Shader const* GetHLSLShader(void)
-{
-	char const* vs_source =
-		"struct VS_INPUT					"
-		"{									"
-		"    float3 pos : POSITION;			"
-		"    float2 uv : TEXCOORD;			"
-		"};									"
-		"									"
-		"struct VS_OUTPUT					"
-		"{									"
-		"    float4 pos : SV_POSITION;		"
-		"    float2 uv : TEXCOORD;			"
-		"};									"
-		"									"
-		"VS_OUTPUT main(VS_INPUT input)		"
-		"{									"
-		"	VS_OUTPUT output;						"
-		"	output.pos = float4(input.pos, 1.0);	"
-		"	output.uv = input.uv;					"
-		"	return output;					"
-		"}									"
-	;
+		return geometry;
+	}
 
-	char const* ps_source =
-		"struct PS_INPUT								"
-		"{												"
-		"    float4 pos : SV_POSITION;					"
-		"    float2 uv : TEXCOORD;						"
-		"};												"
-		"												"
-		"Texture2D uTexture : register(t0);				"
-		"SamplerState uSampler : register(s0);			"
-		"												"
-		"float4 main(PS_INPUT input) : SV_TARGET		"
-		"{												"
-		"	return uTexture.Sample(uSampler, input.uv);	"
-		"}												"
-	;
-
-	static bksge::Shader const shader
+	static bksge::Shader const* GetGLSLShader(void)
 	{
-		bksge::ShaderType::kHLSL,
-		{
-			{ bksge::ShaderStage::kVertex,   vs_source },
-			{ bksge::ShaderStage::kFragment, ps_source },
-		}
-	};
+		static char const* vs_source =
+			"attribute vec3 aPosition;					"
+			"attribute vec2 aTexCoord;					"
+			"varying   vec2 vTexCoord;					"
+			"											"
+			"uniform mat4 uMatrix;						"
+			"											"
+			"void main()								"
+			"{											"
+			"	gl_Position = uMatrix * vec4(aPosition, 1.0);	"
+			"	vTexCoord   = aTexCoord;				"
+			"}											"
+		;
 
-	return &shader;
-}
+		static char const* fs_source =
+			"varying vec2 vTexCoord;					"
+			"uniform sampler2D uSampler;				"
+			"											"
+			"void main()								"
+			"{											"
+			"	gl_FragColor = texture2D(uSampler, vTexCoord);	"
+			"}											"
+		;
+
+		static bksge::Shader const shader
+		{
+			bksge::ShaderType::kGLSL, 
+			{
+				{ bksge::ShaderStage::kVertex,   vs_source },
+				{ bksge::ShaderStage::kFragment, fs_source },
+			}
+		};
+
+		return &shader;
+	}
+
+	static bksge::Shader const* GetHLSLShader(void)
+	{
+		static char const* vs_source =
+			"struct VS_INPUT					"
+			"{									"
+			"    float3 pos : POSITION;			"
+			"    float2 uv : TEXCOORD;			"
+			"};									"
+			"									"
+			"struct VS_OUTPUT					"
+			"{									"
+			"    float4 pos : SV_POSITION;		"
+			"    float2 uv : TEXCOORD;			"
+			"};									"
+			"									"
+			"cbuffer ConstantBuffer				"
+			"{									"
+			"	row_major float4x4 uMatrix;		"
+			"};									"
+			"									"
+			"VS_OUTPUT main(VS_INPUT input)		"
+			"{									"
+			"	VS_OUTPUT output;						"
+			"	output.pos = mul(float4(input.pos, 1.0), uMatrix);	"
+			"	output.uv = input.uv;					"
+			"	return output;					"
+			"}									"
+		;
+
+		static char const* ps_source =
+			"struct PS_INPUT								"
+			"{												"
+			"    float4 pos : SV_POSITION;					"
+			"    float2 uv : TEXCOORD;						"
+			"};												"
+			"												"
+			"Texture2D uTexture : register(t0);				"
+			"SamplerState uSampler : register(s0);			"
+			"												"
+			"float4 main(PS_INPUT input) : SV_TARGET		"
+			"{												"
+			"	return uTexture.Sample(uSampler, input.uv);	"
+			"}												"
+		;
+
+		static bksge::Shader const shader
+		{
+			bksge::ShaderType::kHLSL, 
+			{
+				{ bksge::ShaderStage::kVertex,   vs_source },
+				{ bksge::ShaderStage::kFragment, ps_source },
+			}
+		};
+
+		return &shader;
+	}
+
+	static std::vector<bksge::Shader const*> const& GetShaderList(void)
+	{
+		static std::vector<bksge::Shader const*> const shader_list
+		{
+			GetGLSLShader(),
+			GetHLSLShader(),
+		};
+
+		return shader_list;
+	}
+
+public:
+	Sprite(bksge::Texture const& texture, float x, float y)
+		: m_sampler(texture)
+		, m_position(x, y, 0.0f)
+		, m_scale(0.5f, 0.5f, 1.0f)
+		, m_rotation(0)
+	{
+		m_sampler.SetMagFilter(bksge::FilterMode::kLinear);
+		m_sampler.SetWrapS(bksge::WrapMode::kClamp);
+		m_sampler.SetWrapT(bksge::WrapMode::kClamp);
+	}
+
+	void Update(void)
+	{
+		m_rotation += 0.01f;
+	}
+
+	void Draw(bksge::Renderer* renderer)
+	{
+		m_shader_parameter.SetParameter("uSampler", m_sampler);
+		m_shader_parameter.SetParameter("uTexture", m_sampler.source());
+		bksge::Matrix4x4f mat =
+			bksge::Matrix4x4f::MakeScale(m_scale) *
+			bksge::Matrix4x4f::MakeRotationZ(m_rotation) *
+			bksge::Matrix4x4f::MakeTranslation(m_position);
+		m_shader_parameter.SetParameter("uMatrix", mat);
+
+		renderer->Render(
+			GetGeometry(),
+			GetShaderList(),
+			m_shader_parameter,
+			m_render_state);
+	}
+
+	bksge::Sampler& sampler(void) { return m_sampler; }
+
+private:
+	bksge::ShaderParameterMap	m_shader_parameter;
+	bksge::RenderState			m_render_state;
+	bksge::Sampler				m_sampler;
+	bksge::Vector3f				m_position;
+	bksge::Scale3f				m_scale;
+	float						m_rotation;
+};
 
 }	// namespace
 
@@ -150,55 +238,68 @@ int main()
 	}
 #endif
 
-	bksge::Vertex<bksge::VPosition, bksge::VTexCoord> const vertices[] =
-	{
-		{{{-0.5,  0.5, 0.0}}, {{0, 0}}},
-		{{{-0.5, -0.5, 0.0}}, {{0, 1}}},
-		{{{ 0.5,  0.5, 0.0}}, {{1, 0}}},
-		{{{ 0.5, -0.5, 0.0}}, {{1, 1}}},
-	};
-	std::uint16_t const indices[] =
-	{
-		0, 1, 2,
-		2, 1, 3,
-	};
+	std::size_t const tex0_width  = 16;
+	std::size_t const tex0_height = 16;
 
-	bksge::Geometry const geometry(
-		bksge::Primitive::kTriangles, vertices, indices);
-
-	std::size_t const tex_width  = 16;
-	std::size_t const tex_height = 16;
-
-	std::uint8_t pixels[tex_width * tex_height * 4];
-	auto* p = pixels;
-	for (std::size_t y = 0; y < tex_height; ++y)
+	std::uint8_t pixels0[tex0_width * tex0_height * 4];
 	{
-		for (std::size_t x = 0; x < tex_width; ++x)
+		auto* p = pixels0;
+		for (std::size_t y = 0; y < tex0_height; ++y)
 		{
-			*p++ = static_cast<std::uint8_t>(x * 16);
-			*p++ = static_cast<std::uint8_t>(y * 16);
-			*p++ = 0;
-			*p++ = 255;
+			for (std::size_t x = 0; x < tex0_width; ++x)
+			{
+				*p++ = static_cast<std::uint8_t>(x * 16);
+				*p++ = static_cast<std::uint8_t>(y * 16);
+				*p++ = 0;
+				*p++ = 255;
+			}
 		}
 	}
-	bksge::Texture const texture(
-		bksge::TextureFormat::kRGBA_U8, {tex_width, tex_height}, pixels);
-	bksge::Sampler sampler(texture);
-	sampler.SetMagFilter(bksge::FilterMode::kLinear);
-	sampler.SetWrapS(bksge::WrapMode::kClamp);
-	sampler.SetWrapT(bksge::WrapMode::kClamp);
+	bksge::Texture const tex0(
+		bksge::TextureFormat::kRGBA_U8, {tex0_width, tex0_height}, pixels0);
 
-	std::vector<bksge::Shader const*> const shader_list
+	std::size_t const tex1_width  = 8;
+	std::size_t const tex1_height = 8;
+
+	std::uint8_t pixels1[tex1_width * tex1_height * 4];
 	{
-		GetGLSLShader(),
-		GetHLSLShader(),
-	};
+		auto* p = pixels1;
+		for (std::size_t y = 0; y < tex1_height; ++y)
+		{
+			for (std::size_t x = 0; x < tex1_width; ++x)
+			{
+				*p++ = static_cast<std::uint8_t>(x * 16);
+				*p++ = static_cast<std::uint8_t>(x * 16);
+				*p++ = static_cast<std::uint8_t>(x * 16);
+				*p++ = 255;
+			}
+		}
+	}
+	bksge::Texture const tex1(
+		bksge::TextureFormat::kRGBA_U8, {tex1_width, tex1_height}, pixels1);
 
-	bksge::ShaderParameterMap shader_parameter;
-	shader_parameter.SetParameter("uSampler", sampler);
-	shader_parameter.SetParameter("uTexture", texture);
+	std::vector<std::shared_ptr<Sprite>>	sprites;
 
-	bksge::RenderState render_state;
+	{
+		auto sprite = std::make_shared<Sprite>(tex0, -0.5f,  0.5f);
+		sprites.push_back(sprite);
+	}
+	{
+		auto sprite = std::make_shared<Sprite>(tex0,  0.5f,  0.5f);
+		sprites.push_back(sprite);
+	}
+	{
+		auto sprite = std::make_shared<Sprite>(tex0, -0.5f, -0.5f);
+		sprite->sampler().SetMagFilter(bksge::FilterMode::kNearest);
+		sprites.push_back(sprite);
+	}
+	{
+		auto sprite = std::make_shared<Sprite>(tex0,  0.5f, -0.5f);
+		sprite->sampler().SetMagFilter(bksge::FilterMode::kNearest);
+		sprites.push_back(sprite);
+	}
+
+	int count = 0;
 
 	for (;;)
 	{
@@ -210,16 +311,35 @@ int main()
 			}
 		}
 
+		if (((count / 60) % 2) == 0)
+		{
+			sprites[0]->sampler().SetSource(tex0);
+			sprites[2]->sampler().SetSource(tex0);
+		}
+		else
+		{
+			sprites[0]->sampler().SetSource(tex1);
+			sprites[2]->sampler().SetSource(tex1);
+		}
+
+		for (auto&& sprite : sprites)
+		{
+			sprite->Update();
+		}
+
 		for (auto& renderer : renderers)
 		{
 			renderer->Begin();
-			renderer->Render(
-				geometry,
-				shader_list,
-				shader_parameter,
-				render_state);
+
+			for (auto&& sprite : sprites)
+			{
+				sprite->Draw(renderer.get());
+			}
+
 			renderer->End();
 		}
+
+		++count;
 	}
 
 	return 0;
