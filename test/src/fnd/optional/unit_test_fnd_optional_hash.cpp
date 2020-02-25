a8587e60cf1e48071ed7ecc8252dbf84da5b8692
myoukaku <myoukaku@hotmail.com>
2020-02-25 16:12:35 +0900

    Fix optional hash test

--- test/src/fnd/optional/unit_test_fnd_optional_hash.cpp
+++ test/src/fnd/optional/unit_test_fnd_optional_hash.cpp
@@ -8,14 +8,17 @@
 
 #include <bksge/fnd/optional/optional.hpp>
 #include <gtest/gtest.h>
+#include <string>
 #include "constexpr_test.hpp"
 
 GTEST_TEST(OptionalTest, HashTest)
 {
+#if 0	// 値を格納していないoptionalのハッシュは未規定
 	{
 		const bksge::optional<int> o;
 		EXPECT_EQ(0u, std::hash<bksge::optional<int>>()(o));
 	}
+#endif
 	{
 		const bksge::optional<int> o(1);
 		EXPECT_EQ(
@@ -27,5 +30,11 @@ GTEST_TEST(OptionalTest, HashTest)
 		EXPECT_EQ(
 			std::hash<int>()(42),
 			std::hash<bksge::optional<int>>()(o));
+	}
+	{
+		const bksge::optional<std::string> o("abc");
+		EXPECT_EQ(
+			std::hash<std::string>()("abc"),
+			std::hash<bksge::optional<std::string>>()(o));
 	}
 }
