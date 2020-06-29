f8f3a63e60e698aa4ab9f65b376030a302df53a7
myoukaku <myoukaku@hotmail.com>
2020-06-29 13:22:48 +0900

    Fix glslang.cmake

--- externals/glslang.cmake
+++ externals/glslang.cmake
@@ -1,6 +1,7 @@
 ﻿message("Configuring glslang")
 
 option(SKIP_GLSLANG_INSTALL "Skip installation" On)
+set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
 add_subdirectory(externals/glslang)
 
 add_library(glslang-default-resource-limits-lib STATIC
