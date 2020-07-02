0e150a02a2f4b8b9bb28942bb1242cd7c3409448
myoukaku <myoukaku@hotmail.com>
2020-07-02 17:48:29 +0900

    Add iter_move

---
+++ include/bksge/fnd/iterator/iter_move.hpp
@@ -0,0 +1,81 @@
+﻿/**
+ *	@file	iter_move.hpp
+ *
+ *	@brief	iter_move の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_ITERATOR_ITER_MOVE_HPP
+#define BKSGE_FND_ITERATOR_ITER_MOVE_HPP
+
+#include <bksge/fnd/type_traits/disjunction.hpp>
+#include <bksge/fnd/type_traits/enable_if.hpp>
+#include <bksge/fnd/type_traits/is_class.hpp>
+#include <bksge/fnd/type_traits/is_enum.hpp>
+#include <bksge/fnd/type_traits/is_lvalue_reference.hpp>
+#include <bksge/fnd/type_traits/is_rvalue_reference.hpp>
+#include <bksge/fnd/type_traits/remove_cvref.hpp>
+#include <bksge/fnd/utility/forward.hpp>
+#include <bksge/fnd/utility/move.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+namespace ranges
+{
+
+namespace iter_move_detail
+{
+
+#define BKSGE_NOEXCEPT_DECLTYPE_RETURN(...) \
+	BKSGE_NOEXCEPT_IF_EXPR(__VA_ARGS__)     \
+	-> decltype(__VA_ARGS__)                \
+	{ return __VA_ARGS__; }
+
+void iter_move();
+
+struct iter_move_fn
+{
+	template <typename T>
+	struct is_class_or_enum
+		: public bksge::disjunction<bksge::is_class<T>, bksge::is_enum<T>>
+	{};
+
+	// (1) if std::remove_cvref_t<T> is a class or enumeration type and
+	//        iter_move(std::forward<T>(t)) is well-formed in unevaluated context
+	template <typename T>
+	BKSGE_CONSTEXPR auto
+	operator()(T&& t, bksge::enable_if_t<is_class_or_enum<bksge::remove_cvref_t<T>>::value>* = nullptr) const
+		BKSGE_NOEXCEPT_DECLTYPE_RETURN(
+			iter_move(bksge::forward<T>(t)))
+
+	// (2) otherwise, if *std::forward<T>(t) is well-formed and is an lvalue
+	template <typename T>
+	BKSGE_CONSTEXPR auto
+	operator()(T&& t, bksge::enable_if_t<bksge::is_lvalue_reference<decltype(*bksge::forward<T>(t))>::value>* = nullptr) const
+		BKSGE_NOEXCEPT_DECLTYPE_RETURN(
+			bksge::move(*bksge::forward<T>(t)))
+
+	// (3) otherwise, if *std::forward<T>(t) is well-formed and is an rvalue
+	template <typename T>
+	BKSGE_CONSTEXPR auto
+	operator()(T&& t, bksge::enable_if_t<bksge::is_rvalue_reference<decltype(*bksge::forward<T>(t))>::value>* = nullptr) const
+		BKSGE_NOEXCEPT_DECLTYPE_RETURN(
+			*bksge::forward<T>(t))
+};
+
+#undef BKSGE_NOEXCEPT_DECLTYPE_RETURN
+
+}	// namespace iter_move_detail
+
+// Customization point object
+BKSGE_INLINE_VAR BKSGE_CONSTEXPR
+auto iter_move = iter_move_detail::iter_move_fn{};
+
+}	// namespace ranges
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_ITERATOR_ITER_MOVE_HPP
---
+++ test/src/fnd/iterator/unit_test_fnd_iterator_iter_move.cpp
@@ -0,0 +1,83 @@
+﻿/**
+ *	@file	unit_test_fnd_iterator_iter_move.cpp
+ *
+ *	@brief	iter_move のテスト
+ *
+ *	@author	myoukaku
+ */
+
+#include <bksge/fnd/iterator/iter_move.hpp>
+#include <gtest/gtest.h>
+#include "constexpr_test.hpp"
+
+namespace bksge_iterator_test
+{
+
+namespace iter_move_test
+{
+
+struct X
+{
+	int value;
+
+	BKSGE_CXX14_CONSTEXPR X(int i): value(i) { }
+
+	X(const X&) = default;
+	X& operator=(const X&) = default;
+
+	BKSGE_CXX14_CONSTEXPR X(X&& x)
+		: value(x.value)
+	{
+		x.value = -2;
+	}
+
+	BKSGE_CXX14_CONSTEXPR X& operator=(X&& x)
+	{
+		value = x.value;
+		x.value = -1;
+		return *this;
+	}
+};
+
+struct CustomIterator
+{
+	X* m_p;
+	BKSGE_CXX14_CONSTEXPR CustomIterator(X* p): m_p(p) {}
+	BKSGE_CXX14_CONSTEXPR X&& operator*() const { return static_cast<X&&>(*m_p); }
+};
+
+struct CustomIterator2
+{
+	X* m_p;
+	BKSGE_CXX14_CONSTEXPR CustomIterator2(X* p): m_p(p) {}
+};
+
+inline BKSGE_CXX14_CONSTEXPR X&&
+iter_move(CustomIterator2 const& i)
+{
+	return static_cast<X&&>(*i.m_p);
+}
+
+template <typename It>
+inline BKSGE_CXX14_CONSTEXPR bool
+test_X(int i, int j)
+{
+	X x1{i}, x2{j};
+	bksge::ranges::iter_move(It(&x1)); // no-op
+	x1 = bksge::ranges::iter_move(It(&x2));
+	return x1.value == j && x2.value == -1;
+}
+
+GTEST_TEST(IteratorTest, IterMoveTest)
+{
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test_X<X*>(1, 2));
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test_X<X*>(3, 4));
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test_X<CustomIterator>(1, 2));
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test_X<CustomIterator>(3, 4));
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test_X<CustomIterator2>(1, 2));
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test_X<CustomIterator2>(3, 4));
+}
+
+}	// namespace iter_move_test
+
+}	// namespace bksge_iterator_test
