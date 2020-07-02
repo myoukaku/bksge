452603eec30bb32adc063554f537272dba8583d9
myoukaku <myoukaku@hotmail.com>
2020-07-02 15:36:01 +0900

    Add iter_reference_t

---
+++ include/bksge/fnd/iterator/iter_reference_t.hpp
@@ -0,0 +1,22 @@
+﻿/**
+ *	@file	iter_reference_t.hpp
+ *
+ *	@brief	iter_reference_t の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_ITERATOR_ITER_REFERENCE_T_HPP
+#define BKSGE_FND_ITERATOR_ITER_REFERENCE_T_HPP
+
+#include <bksge/fnd/utility/declval.hpp>
+
+namespace bksge
+{
+
+template <typename I>	// dereferenceable
+using iter_reference_t = decltype(*bksge::declval<I&>());
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_ITERATOR_ITER_REFERENCE_T_HPP
