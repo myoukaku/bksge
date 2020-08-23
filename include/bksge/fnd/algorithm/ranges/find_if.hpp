da903676b6f9d56ddfcd3b162828c871e6319255
myoukaku <myoukaku@hotmail.com>
2020-08-23 12:50:30 +0900

    Add ranges::find_if

---
+++ include/bksge/fnd/algorithm/ranges/find_if.hpp
@@ -0,0 +1,100 @@
+﻿/**
+ *	@file	find_if.hpp
+ *
+ *	@brief	ranges::find_if の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_ALGORITHM_RANGES_FIND_IF_HPP
+#define BKSGE_FND_ALGORITHM_RANGES_FIND_IF_HPP
+
+#include <bksge/fnd/functional/identity.hpp>
+#include <bksge/fnd/functional/invoke.hpp>
+#include <bksge/fnd/iterator/concepts/input_iterator.hpp>
+#include <bksge/fnd/iterator/concepts/sentinel_for.hpp>
+#include <bksge/fnd/iterator/concepts/indirect_unary_predicate.hpp>
+#include <bksge/fnd/iterator/projected.hpp>
+#include <bksge/fnd/ranges/concepts/input_range.hpp>
+#include <bksge/fnd/ranges/iterator_t.hpp>
+#include <bksge/fnd/ranges/borrowed_iterator_t.hpp>
+#include <bksge/fnd/ranges/begin.hpp>
+#include <bksge/fnd/ranges/end.hpp>
+#include <bksge/fnd/type_traits/enable_if.hpp>
+#include <bksge/fnd/type_traits/conjunction.hpp>
+#include <bksge/fnd/utility/move.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+namespace ranges
+{
+
+struct find_if_fn
+{
+	template <
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+		bksge::input_iterator Iter,
+		bksge::sentinel_for<Iter> Sent,
+		typename Proj = bksge::identity,
+		bksge::indirect_unary_predicate<bksge::projected<Iter, Proj>> Pred
+#else
+		typename Iter,
+		typename Sent,
+		typename Proj = bksge::identity,
+		typename Pred,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			bksge::input_iterator<Iter>,
+			bksge::sentinel_for<Sent, Iter>,
+			bksge::indirect_unary_predicate<Pred, bksge::projected<Iter, Proj>>
+		>::value>
+#endif
+	>
+	BKSGE_CXX14_CONSTEXPR Iter operator()(
+		Iter first, Sent last, Pred pred, Proj proj = {}) const
+	{
+		while (first != last &&
+			!(bool)bksge::invoke(pred, bksge::invoke(proj, *first)))
+		{
+			++first;
+		}
+
+		return first;
+	}
+
+	template <
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+		ranges::input_range Range,
+		typename Proj = bksge::identity,
+		bksge::indirect_unary_predicate<bksge::projected<ranges::iterator_t<Range>, Proj>> Pred
+#else
+		typename Range,
+		typename Proj = bksge::identity,
+		typename Pred,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			ranges::input_range<Range>,
+			bksge::indirect_unary_predicate<Pred, bksge::projected<ranges::iterator_t<Range>, Proj>>
+		>::value>
+#endif
+	>
+	BKSGE_CXX14_CONSTEXPR ranges::borrowed_iterator_t<Range>
+		operator()(Range&& r, Pred pred, Proj proj = {}) const
+	{
+		return (*this)(
+			ranges::begin(r), ranges::end(r), bksge::move(pred), bksge::move(proj));
+	}
+};
+
+inline namespace cpo
+{
+
+BKSGE_INLINE_VAR BKSGE_CONSTEXPR find_if_fn find_if{};
+
+}	// inline namespace cpo
+
+}	// namespace ranges
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_ALGORITHM_RANGES_FIND_IF_HPP
