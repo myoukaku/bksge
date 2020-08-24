13d528bc8c550e1370608acb56c6be580360ea00
myoukaku <myoukaku@hotmail.com>
2020-08-24 11:18:50 +0900

    Add ranges::mismatch

---
+++ include/bksge/fnd/algorithm/ranges/in_in_result.hpp
@@ -0,0 +1,71 @@
+﻿/**
+ *	@file	in_in_result.hpp
+ *
+ *	@brief	ranges::in_in_result の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_ALGORITHM_RANGES_IN_IN_RESULT_HPP
+#define BKSGE_FND_ALGORITHM_RANGES_IN_IN_RESULT_HPP
+
+#include <bksge/fnd/concepts/convertible_to.hpp>
+#include <bksge/fnd/type_traits/enable_if.hpp>
+#include <bksge/fnd/type_traits/conjunction.hpp>
+#include <bksge/fnd/utility/move.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+namespace ranges
+{
+
+template <typename Iter1, typename Iter2>
+struct in_in_result
+{
+	/*[[no_unique_address]]*/ Iter1 in1;
+	/*[[no_unique_address]]*/ Iter2 in2;
+
+	template <typename IIter1, typename IIter2
+#if !defined(BKSGE_HAS_CXX20_CONCEPTS)
+		, typename = bksge::enable_if_t<bksge::conjunction<
+			bksge::convertible_to<Iter1 const&, IIter1>,
+			bksge::convertible_to<Iter2 const&, IIter2>
+		>::value>
+	>
+#else
+	>
+	requires
+		bksge::convertible_to<Iter1 const&, IIter1> &&
+		bksge::convertible_to<Iter2 const&, IIter2>
+#endif
+	BKSGE_CXX14_CONSTEXPR operator in_in_result<IIter1, IIter2>() const&
+	{
+		return { in1, in2 };
+	}
+
+	template <typename IIter1, typename IIter2
+#if !defined(BKSGE_HAS_CXX20_CONCEPTS)
+		, typename = bksge::enable_if_t<bksge::conjunction<
+			bksge::convertible_to<Iter1, IIter1>,
+			bksge::convertible_to<Iter2, IIter2>
+		>::value>
+	>
+#else
+	>
+	requires
+		bksge::convertible_to<Iter1, IIter1> &&
+		bksge::convertible_to<Iter2, IIter2>
+#endif
+	BKSGE_CXX14_CONSTEXPR operator in_in_result<IIter1, IIter2>() &&
+	{
+		return { bksge::move(in1), bksge::move(in2) };
+	}
+};
+
+}	// namespace ranges
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_ALGORITHM_RANGES_IN_IN_RESULT_HPP
---
+++ include/bksge/fnd/algorithm/ranges/mismatch.hpp
@@ -0,0 +1,143 @@
+﻿/**
+ *	@file	mismatch.hpp
+ *
+ *	@brief	ranges::mismatch の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_ALGORITHM_RANGES_MISMATCH_HPP
+#define BKSGE_FND_ALGORITHM_RANGES_MISMATCH_HPP
+
+#include <bksge/fnd/algorithm/ranges/in_in_result.hpp>
+#include <bksge/fnd/functional/equal_to.hpp>
+#include <bksge/fnd/functional/identity.hpp>
+#include <bksge/fnd/functional/invoke.hpp>
+#include <bksge/fnd/iterator/concepts/input_iterator.hpp>
+#include <bksge/fnd/iterator/concepts/sentinel_for.hpp>
+#include <bksge/fnd/iterator/concepts/indirectly_comparable.hpp>
+#include <bksge/fnd/ranges/concepts/input_range.hpp>
+#include <bksge/fnd/ranges/iterator_t.hpp>
+#include <bksge/fnd/ranges/begin.hpp>
+#include <bksge/fnd/ranges/end.hpp>
+#include <bksge/fnd/type_traits/enable_if.hpp>
+#include <bksge/fnd/type_traits/conjunction.hpp>
+#include <bksge/fnd/utility/move.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+namespace ranges
+{
+
+template <typename Iter1, typename Iter2>
+using mismatch_result = in_in_result<Iter1, Iter2>;
+
+struct mismatch_fn
+{
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+	template <
+		bksge::input_iterator Iter1, bksge::sentinel_for<Iter1> Sent1,
+		bksge::input_iterator Iter2, bksge::sentinel_for<Iter2> Sent2,
+		typename Pred = ranges::equal_to,
+		typename Proj1 = bksge::identity,
+		typename Proj2 = bksge::identity
+	>
+	requires bksge::indirectly_comparable<
+		Iter1, Iter2, Pred, Proj1, Proj2>
+#else
+	template <
+		typename Iter1, typename Sent1,
+		typename Iter2, typename Sent2,
+		typename Pred = ranges::equal_to,
+		typename Proj1 = bksge::identity,
+		typename Proj2 = bksge::identity,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			bksge::input_iterator<Iter1>, bksge::sentinel_for<Sent1, Iter1>,
+			bksge::input_iterator<Iter2>, bksge::sentinel_for<Sent2, Iter2>,
+			bksge::indirectly_comparable<
+				Iter1, Iter2, Pred, Proj1, Proj2>
+		>::value>
+	>
+#endif
+	BKSGE_CXX14_CONSTEXPR mismatch_result<Iter1, Iter2>
+	operator()(
+		Iter1 first1, Sent1 last1,
+		Iter2 first2, Sent2 last2,
+		Pred pred = {},
+		Proj1 proj1 = {},
+		Proj2 proj2 = {}) const
+	{
+		while (first1 != last1 && first2 != last2 &&
+			(bool)bksge::invoke(pred,
+				bksge::invoke(proj1, *first1),
+				bksge::invoke(proj2, *first2)))
+		{
+			++first1;
+			++first2;
+		}
+
+		return { bksge::move(first1), bksge::move(first2) };
+	}
+
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+	template <
+		ranges::input_range Range1,
+		ranges::input_range Range2,
+		typename Pred = ranges::equal_to,
+		typename Proj1 = bksge::identity,
+		typename Proj2 = bksge::identity
+	>
+	requires bksge::indirectly_comparable<
+		ranges::iterator_t<Range1>,
+		ranges::iterator_t<Range2>,
+		Pred, Proj1, Proj2>
+#else
+	template <
+		typename Range1,
+		typename Range2,
+		typename Pred = ranges::equal_to,
+		typename Proj1 = bksge::identity,
+		typename Proj2 = bksge::identity,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			ranges::input_range<Range1>,
+			ranges::input_range<Range2>,
+			bksge::indirectly_comparable<
+				ranges::iterator_t<Range1>,
+				ranges::iterator_t<Range2>,
+				Pred, Proj1, Proj2>
+		>::value>
+	>
+#endif
+	BKSGE_CXX14_CONSTEXPR mismatch_result<
+		ranges::iterator_t<Range1>,
+		ranges::iterator_t<Range2>>
+	operator()(
+		Range1&& r1,
+		Range2&& r2,
+		Pred pred = {},
+		Proj1 proj1 = {},
+		Proj2 proj2 = {}) const
+	{
+		return (*this)(
+			ranges::begin(r1), ranges::end(r1),
+			ranges::begin(r2), ranges::end(r2),
+			bksge::move(pred),
+			bksge::move(proj1),
+			bksge::move(proj2));
+	}
+};
+
+inline namespace cpo
+{
+
+BKSGE_INLINE_VAR BKSGE_CONSTEXPR mismatch_fn mismatch{};
+
+}	// inline namespace cpo
+
+}	// namespace ranges
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_ALGORITHM_RANGES_MISMATCH_HPP
