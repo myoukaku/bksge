ac929cb4824d6f07b9fd39338c52c2fc20762849
myoukaku <myoukaku@hotmail.com>
2020-08-26 16:14:55 +0900

    Add ranges::fill_n

---
+++ include/bksge/fnd/algorithm/ranges/fill_n.hpp
@@ -0,0 +1,100 @@
+﻿/**
+ *	@file	fill_n.hpp
+ *
+ *	@brief	ranges::fill_n の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_ALGORITHM_RANGES_FILL_N_HPP
+#define BKSGE_FND_ALGORITHM_RANGES_FILL_N_HPP
+
+#include <bksge/fnd/concepts/detail/require.hpp>
+#include <bksge/fnd/concepts/detail/overload_priority.hpp>
+#include <bksge/fnd/iterator/concepts/output_iterator.hpp>
+#include <bksge/fnd/iterator/iter_difference_t.hpp>
+#include <bksge/fnd/type_traits/enable_if.hpp>
+#include <bksge/fnd/type_traits/is_scalar.hpp>
+#include <bksge/fnd/utility/move.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+namespace ranges
+{
+
+struct fill_n_fn
+{
+private:
+	//// TODO: Generalize this optimization to contiguous iterators.
+	//if constexpr (is_pointer_v<Out>
+	//	// Note that __is_byte already implies !is_volatile.
+	//	&& __is_byte<remove_pointer_t<Out>>::value
+	//	&& integral<T>)
+	//{
+	//	__builtin_memset(first, static_cast<unsigned char>(value), n);
+	//	return first + n;
+	//}
+
+	template <typename T, typename Out, typename Difference,
+		typename = bksge::enable_if_t<bksge::is_scalar<T>::value>>
+	static BKSGE_CXX14_CONSTEXPR Out
+	impl(Out first, Difference n, T const& value,
+		bksge::detail::overload_priority<1>)
+	{
+		auto const tmp = value;
+		for (; n > 0; --n, (void)++first)
+		{
+			*first = tmp;
+		}
+
+		return first;
+	}
+
+	template <typename T, typename Out, typename Difference>
+	static BKSGE_CXX14_CONSTEXPR Out
+	impl(Out first, Difference n, T const& value,
+		bksge::detail::overload_priority<0>)
+	{
+		for (; n > 0; --n, (void)++first)
+		{
+			*first = value;
+		}
+
+		return first;
+	}
+
+public:
+	template <
+		typename T,
+		BKSGE_REQUIRES_PARAM_2(bksge::output_iterator, T const&, Out)
+	>
+	BKSGE_CXX14_CONSTEXPR Out operator()(
+		Out first, bksge::iter_difference_t<Out> n, T const& value) const
+	{
+		if (n <= 0)
+		{
+			return first;
+		}
+
+		return impl(
+			bksge::move(first),
+			bksge::move(n),
+			value,
+			bksge::detail::overload_priority<1>{});
+	}
+};
+
+inline namespace cpo
+{
+
+BKSGE_INLINE_VAR BKSGE_CONSTEXPR fill_n_fn fill_n{};
+
+}	// inline namespace cpo
+
+}	// namespace ranges
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_ALGORITHM_RANGES_FILL_N_HPP
---
+++ test/src/fnd/algorithm/ranges/unit_test_fnd_algorithm_ranges_fill_n.cpp
@@ -0,0 +1,88 @@
+﻿/**
+ *	@file	unit_test_fnd_algorithm_ranges_fill_n.cpp
+ *
+ *	@brief	ranges::fill_n のテスト
+ *
+ *	@author	myoukaku
+ */
+
+#include <bksge/fnd/algorithm/ranges/fill_n.hpp>
+#include <bksge/fnd/algorithm/ranges/equal.hpp>
+#include <bksge/fnd/iterator/ranges/next.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "constexpr_test.hpp"
+#include "ranges_test.hpp"
+
+namespace bksge_algorithm_test
+{
+
+namespace ranges_fill_n_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+inline BKSGE_CXX14_CONSTEXPR bool test01()
+{
+	namespace ranges = bksge::ranges;
+	{
+		int x[] = { 1, 2, 3, 4, 5, 6 };
+		auto res = ranges::fill_n(x, 3, 17);
+		VERIFY(res == ranges::next(x, 3));
+		int const y[] = { 17, 17, 17, 4, 5, 6 };
+		VERIFY(ranges::equal(x, y));
+	}
+	{
+		int x[] = { 1, 2, 3, 4, 5, 6 };
+		auto res = ranges::fill_n(x, -1, 17);
+		VERIFY(res == x);
+		int const y[] = { 1, 2, 3, 4, 5, 6 };
+		VERIFY(ranges::equal(x, y));
+	}
+	{
+		int x[] = { 1, 2, 3, 4, 5, 6 };
+		test_range<int, output_iterator_wrapper> rx(x);
+		auto res = ranges::fill_n(rx.begin(), 3, 17);
+		VERIFY(res.m_ptr == ranges::next(x, 3));
+		int const y[] = { 17, 17, 17, 4, 5, 6 };
+		VERIFY(ranges::equal(x, y));
+	}
+	return true;
+}
+
+struct X
+{
+	int i;
+};
+
+inline bool test02()
+{
+	namespace ranges = bksge::ranges;
+	{
+		X x[6] = { {1}, {2}, {3}, {4}, {5}, {6} };
+		auto res = ranges::fill_n(x, 3, X{ 17 });
+		VERIFY(res == ranges::next(x, 3));
+		int const y[] = { 17, 17, 17, 4, 5, 6 };
+		VERIFY(ranges::equal(x, y, {}, &X::i, {}));
+	}
+	{
+		std::vector<int> x(5);
+		auto res = ranges::fill_n(x.begin(), 2, 42);
+		VERIFY(res == ranges::next(x.begin(), 2));
+		int const y[] = { 42, 42, 0, 0, 0 };
+		VERIFY(ranges::equal(x, y));
+	}
+	return true;
+}
+
+#undef VERIFY
+
+GTEST_TEST(AlgorithmTest, RangesFillNTest)
+{
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test01());
+	EXPECT_TRUE(test02());
+}
+
+}	// namespace ranges_fill_n_test
+
+}	// namespace bksge_algorithm_test
