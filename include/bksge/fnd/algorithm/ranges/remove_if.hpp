e0d26f53c742511cbeb6d29e6da2259dd7da9597
myoukaku <myoukaku@hotmail.com>
2020-08-26 23:17:14 +0900

    Add ranges::remove_if

---
+++ include/bksge/fnd/algorithm/ranges/remove_if.hpp
@@ -0,0 +1,128 @@
+﻿/**
+ *	@file	remove_if.hpp
+ *
+ *	@brief	ranges::remove_if の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_ALGORITHM_RANGES_REMOVE_IF_HPP
+#define BKSGE_FND_ALGORITHM_RANGES_REMOVE_IF_HPP
+
+#include <bksge/fnd/algorithm/ranges/find_if.hpp>
+#include <bksge/fnd/functional/identity.hpp>
+#include <bksge/fnd/functional/invoke.hpp>
+#include <bksge/fnd/iterator/concepts/permutable.hpp>
+#include <bksge/fnd/iterator/concepts/sentinel_for.hpp>
+#include <bksge/fnd/iterator/concepts/indirect_unary_predicate.hpp>
+#include <bksge/fnd/iterator/projected.hpp>
+#include <bksge/fnd/ranges/views/subrange.hpp>
+#include <bksge/fnd/ranges/concepts/forward_range.hpp>
+#include <bksge/fnd/ranges/iterator_t.hpp>
+#include <bksge/fnd/ranges/borrowed_subrange_t.hpp>
+#include <bksge/fnd/ranges/begin.hpp>
+#include <bksge/fnd/ranges/end.hpp>
+#include <bksge/fnd/type_traits/enable_if.hpp>
+#include <bksge/fnd/type_traits/conjunction.hpp>
+#include <bksge/fnd/utility/move.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+namespace ranges
+{
+
+struct remove_if_fn
+{
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+	template <
+		bksge::permutable Iter,
+		bksge::sentinel_for<Iter> Sent,
+		typename Proj = bksge::identity,
+		bksge::indirect_unary_predicate<
+			bksge::projected<Iter, Proj>
+		> Pred
+	>
+#else
+	template <
+		typename Iter,
+		typename Sent,
+		typename Proj = bksge::identity,
+		typename Pred,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			bksge::permutable<Iter>,
+			bksge::sentinel_for<Sent, Iter>,
+			bksge::indirect_unary_predicate<Pred,
+				bksge::projected<Iter, Proj>
+			>
+		>::value>
+	>
+#endif
+	BKSGE_CXX14_CONSTEXPR ranges::subrange<Iter>
+	operator()(
+		Iter first, Sent last, Pred pred, Proj proj = {}) const
+	{
+		first = ranges::find_if(first, last, pred, proj);
+		if (first == last)
+		{
+			return { first, first };
+		}
+
+		auto result = first;
+		++first;
+		for (; first != last; ++first)
+		{
+			if (!bksge::invoke(pred, bksge::invoke(proj, *first)))
+			{
+				*result = bksge::move(*first);
+				++result;
+			}
+		}
+
+		return { result, first };
+	}
+
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+	template <
+		ranges::forward_range Range,
+		typename Proj = bksge::identity,
+		bksge::indirect_unary_predicate<
+			bksge::projected<ranges::iterator_t<Range>, Proj>
+		> Pred
+	>
+	requires bksge::permutable<ranges::iterator_t<Range>>
+#else
+	template <
+		typename Range,
+		typename Proj = bksge::identity,
+		typename Pred,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			ranges::forward_range<Range>,
+			bksge::indirect_unary_predicate<Pred,
+				bksge::projected<ranges::iterator_t<Range>, Proj>>,
+			bksge::permutable<ranges::iterator_t<Range>>
+		>::value>
+	>
+#endif
+	BKSGE_CXX14_CONSTEXPR ranges::borrowed_subrange_t<Range>
+	operator()(Range&& r, Pred pred, Proj proj = {}) const
+	{
+		return (*this)(
+			ranges::begin(r), ranges::end(r),
+			bksge::move(pred), bksge::move(proj));
+	}
+};
+
+inline namespace cpo
+{
+
+BKSGE_INLINE_VAR BKSGE_CONSTEXPR remove_if_fn remove_if{};
+
+}	// inline namespace cpo
+
+}	// namespace ranges
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_ALGORITHM_RANGES_REMOVE_IF_HPP
---
+++ test/src/fnd/algorithm/ranges/unit_test_fnd_algorithm_ranges_remove_if.cpp
@@ -0,0 +1,112 @@
+﻿/**
+ *	@file	unit_test_fnd_algorithm_ranges_remove_if.cpp
+ *
+ *	@brief	ranges::remove_if のテスト
+ *
+ *	@author	myoukaku
+ */
+
+#include <bksge/fnd/algorithm/ranges/remove_if.hpp>
+#include <bksge/fnd/algorithm/ranges/equal.hpp>
+#include <bksge/fnd/iterator/ranges/next.hpp>
+#include <gtest/gtest.h>
+#include "constexpr_test.hpp"
+#include "ranges_test.hpp"
+
+namespace bksge_algorithm_test
+{
+
+namespace ranges_remove_if_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+struct IsEven
+{
+	constexpr bool operator()(int i) const { return i % 2 == 0; }
+};
+
+struct IsOdd
+{
+	constexpr bool operator()(int i) const { return i % 2 == 1; }
+};
+
+inline BKSGE_CXX14_CONSTEXPR bool test01()
+{
+	namespace ranges = bksge::ranges;
+	{
+		int x[] = { 1,2,3,4,5 };
+		auto res = ranges::remove_if(x, x+5, IsEven{});
+		VERIFY(res.begin() == x+3 && res.end() == x+5);
+		int const y[] = { 1,3,5 };
+		VERIFY(ranges::equal(x, x+3, y, y+3));
+	}
+	{
+		int x[] = { 1,2,3,4,5 };
+		test_range<int, forward_iterator_wrapper> rx(x);
+		auto res = ranges::remove_if(rx, IsOdd{});
+		VERIFY(res.begin() == ranges::next(rx.begin(), 2) && res.end() == rx.end());
+		int const y[] = { 2,4 };
+		VERIFY(ranges::equal(x, x+2, y, y+2));
+	}
+	return true;
+}
+
+struct X
+{
+	int i;
+};
+
+inline bool test02()
+{
+	namespace ranges = bksge::ranges;
+	{
+		int x[] = { 1,2,3,4,5 };
+		auto res = ranges::remove_if(x, x+5, [](int a) {return a == 3; });
+		VERIFY(res.begin() == x+4 && res.end() == x+5);
+		int const y[4] = { 1,2,4,5 };
+		VERIFY(ranges::equal(x, x+4, y, y+4));
+	}
+	{
+		int x[] = { 1,2,3,4,5 };
+		auto res = ranges::remove_if(x, x+5, [](int a) {return a == 9; });
+		VERIFY(res.begin() == x+5 && res.end() == x+5);
+		int const y[5] = { 1,2,3,4,5 };
+		VERIFY(ranges::equal(x, x+5, y, y+5));
+	}
+	{
+		int x[] = { 1,2,3,4,5 };
+		test_range<int, forward_iterator_wrapper> rx(x);
+		auto res = ranges::remove_if(rx, [](int a) {return a % 2 == 1; });
+		VERIFY(res.begin() == ranges::next(rx.begin(), 2) && res.end() == rx.end());
+		int const y[2] = { 2,4 };
+		VERIFY(ranges::equal(x, x+2, y, y+2));
+	}
+	{
+		int x[] = { 1,2,3,4,5 };
+		test_range<int, forward_iterator_wrapper> rx(x);
+		auto res = ranges::remove_if(rx, [](int a) {return a < 9; });
+		VERIFY(res.begin() == rx.begin() && res.end() == rx.end());
+	}
+	{
+		X x[] = { {3},{1},{4},{1},{5} };
+		test_range<X, forward_iterator_wrapper> rx(x);
+		auto res = ranges::remove_if(rx, [](int a) {return a < 4; }, &X::i);
+		VERIFY(res.begin() == ranges::next(rx.begin(), 2) && res.end() == rx.end());
+		int const y[2] = { 4,5 };
+		VERIFY(ranges::equal(x, x+2, y, y+2, {}, &X::i));
+	}
+	return true;
+}
+
+#undef VERIFY
+
+GTEST_TEST(AlgorithmTest, RangesRemoveIfTest)
+{
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test01());
+	EXPECT_TRUE(test02());
+}
+
+}	// namespace ranges_remove_if_test
+
+}	// namespace bksge_algorithm_test
