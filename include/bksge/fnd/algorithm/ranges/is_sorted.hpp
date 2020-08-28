060e2dce4a9345299bc2f708509dd865a4049fee
myoukaku <myoukaku@hotmail.com>
2020-08-28 15:51:57 +0900

    Add ranges::is_sorted

---
+++ include/bksge/fnd/algorithm/ranges/is_sorted.hpp
@@ -0,0 +1,120 @@
+﻿/**
+ *	@file	is_sorted.hpp
+ *
+ *	@brief	ranges::is_sorted の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_ALGORITHM_RANGES_IS_SORTED_HPP
+#define BKSGE_FND_ALGORITHM_RANGES_IS_SORTED_HPP
+
+#include <bksge/fnd/functional/ranges/less.hpp>
+#include <bksge/fnd/functional/identity.hpp>
+#include <bksge/fnd/functional/invoke.hpp>
+#include <bksge/fnd/iterator/concepts/forward_iterator.hpp>
+#include <bksge/fnd/iterator/concepts/sentinel_for.hpp>
+#include <bksge/fnd/iterator/concepts/indirect_strict_weak_order.hpp>
+#include <bksge/fnd/iterator/projected.hpp>
+#include <bksge/fnd/ranges/concepts/forward_range.hpp>
+#include <bksge/fnd/ranges/iterator_t.hpp>
+#include <bksge/fnd/ranges/begin.hpp>
+#include <bksge/fnd/ranges/end.hpp>
+#include <bksge/fnd/type_traits/enable_if.hpp>
+#include <bksge/fnd/type_traits/conjunction.hpp>
+#include <bksge/fnd/utility/move.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+namespace ranges
+{
+
+struct is_sorted_fn
+{
+	template <
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+		bksge::forward_iterator Iter,
+		bksge::sentinel_for<Iter> Sent,
+		typename Proj = bksge::identity,
+		bksge::indirect_strict_weak_order<
+			bksge::projected<Iter, Proj>
+		> Comp = ranges::less
+#else
+		typename Iter,
+		typename Sent,
+		typename Proj = bksge::identity,
+		typename Comp = ranges::less,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			bksge::forward_iterator<Iter>,
+			bksge::sentinel_for<Sent, Iter>,
+			bksge::indirect_strict_weak_order<Comp,
+				bksge::projected<Iter, Proj>
+			>
+		>::value>
+#endif
+	>
+	BKSGE_CXX14_CONSTEXPR bool
+	operator()(Iter first, Sent last,
+		Comp comp = {}, Proj proj = {}) const
+	{
+		if (first == last)
+		{
+			return true;
+		}
+
+		auto next = first;
+		for (++next; next != last; first = next, (void)++next)
+		{
+			if (bksge::invoke(comp,
+				bksge::invoke(proj, *next),
+				bksge::invoke(proj, *first)))
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	template <
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+		ranges::forward_range Range,
+		typename Proj = bksge::identity,
+		bksge::indirect_strict_weak_order<
+			bksge::projected<ranges::iterator_t<Range>, Proj>
+		> Comp = ranges::less
+#else
+		typename Range,
+		typename Proj = bksge::identity,
+		typename Comp = ranges::less,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			ranges::forward_range<Range>,
+			bksge::indirect_strict_weak_order<Comp,
+				bksge::projected<ranges::iterator_t<Range>, Proj>
+			>
+		>::value>
+#endif
+	>
+	BKSGE_CXX14_CONSTEXPR bool
+	operator()(Range&& r, Comp comp = {}, Proj proj = {}) const
+	{
+		return (*this)(
+			ranges::begin(r), ranges::end(r),
+			bksge::move(comp), bksge::move(proj));
+	}
+};
+
+inline namespace cpo
+{
+
+BKSGE_INLINE_VAR BKSGE_CONSTEXPR is_sorted_fn is_sorted{};
+
+}	// inline namespace cpo
+
+}	// namespace ranges
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_ALGORITHM_RANGES_IS_SORTED_HPP
