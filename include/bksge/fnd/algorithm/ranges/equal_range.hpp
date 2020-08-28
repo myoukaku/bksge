bbd97af05fedd18c3fbe9b3c01632de28aee34b0
myoukaku <myoukaku@hotmail.com>
2020-08-28 23:36:13 +0900

    Add ranges::equal_range

---
+++ include/bksge/fnd/algorithm/ranges/equal_range.hpp
@@ -0,0 +1,143 @@
+﻿/**
+ *	@file	equal_range.hpp
+ *
+ *	@brief	ranges::equal_range の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_ALGORITHM_RANGES_EQUAL_RANGE_HPP
+#define BKSGE_FND_ALGORITHM_RANGES_EQUAL_RANGE_HPP
+
+#include <bksge/fnd/algorithm/ranges/lower_bound.hpp>
+#include <bksge/fnd/algorithm/ranges/upper_bound.hpp>
+#include <bksge/fnd/functional/ranges/less.hpp>
+#include <bksge/fnd/functional/identity.hpp>
+#include <bksge/fnd/functional/invoke.hpp>
+#include <bksge/fnd/iterator/concepts/forward_iterator.hpp>
+#include <bksge/fnd/iterator/concepts/sentinel_for.hpp>
+#include <bksge/fnd/iterator/concepts/indirect_strict_weak_order.hpp>
+#include <bksge/fnd/iterator/ranges/distance.hpp>
+#include <bksge/fnd/iterator/ranges/advance.hpp>
+#include <bksge/fnd/iterator/projected.hpp>
+#include <bksge/fnd/ranges/concepts/forward_range.hpp>
+#include <bksge/fnd/ranges/views/subrange.hpp>
+#include <bksge/fnd/ranges/iterator_t.hpp>
+#include <bksge/fnd/ranges/borrowed_subrange_t.hpp>
+#include <bksge/fnd/ranges/begin.hpp>
+#include <bksge/fnd/ranges/end.hpp>
+#include <bksge/fnd/type_traits/enable_if.hpp>
+#include <bksge/fnd/type_traits/conjunction.hpp>
+#include <bksge/fnd/utility/move.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+namespace ranges
+{
+
+struct equal_range_fn
+{
+	template <
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+		bksge::forward_iterator Iter,
+		bksge::sentinel_for<Iter> Sent,
+		typename T,
+		typename Proj = bksge::identity,
+		bksge::indirect_strict_weak_order<
+			T const*, bksge::projected<Iter, Proj>
+		> Comp = ranges::less
+#else
+		typename Iter,
+		typename Sent,
+		typename T,
+		typename Proj = bksge::identity,
+		typename Comp = ranges::less,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			bksge::forward_iterator<Iter>,
+			bksge::sentinel_for<Sent, Iter>,
+			bksge::indirect_strict_weak_order<Comp,
+				T const*, bksge::projected<Iter, Proj>>
+		>::value>
+#endif
+	>
+	BKSGE_CXX14_CONSTEXPR ranges::subrange<Iter>
+	operator()(
+		Iter first, Sent last,
+		T const& value, Comp comp = {}, Proj proj = {}) const
+	{
+		auto len = ranges::distance(first, last);
+
+		while (len > 0)
+		{
+			auto half = len / 2;
+			auto middle = first;
+			ranges::advance(middle, half);
+			if (bksge::invoke(comp, bksge::invoke(proj, *middle), value))
+			{
+				first = middle;
+				++first;
+				len = len - half - 1;
+			}
+			else if (bksge::invoke(comp, value, bksge::invoke(proj, *middle)))
+			{
+				len = half;
+			}
+			else
+			{
+				auto left = ranges::lower_bound(
+					first, middle, value, comp, proj);
+				ranges::advance(first, len);
+				auto right = ranges::upper_bound(
+					++middle, first, value, comp, proj);
+				return { left, right };
+			}
+		}
+
+		return { first, first };
+	}
+
+	template <
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+		ranges::forward_range Range,
+		typename T,
+		typename Proj = bksge::identity,
+		bksge::indirect_strict_weak_order<
+			T const*,
+			bksge::projected<ranges::iterator_t<Range>, Proj>
+		> Comp = ranges::less
+#else
+		typename Range,
+		typename T,
+		typename Proj = bksge::identity,
+		typename Comp = ranges::less,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			ranges::forward_range<Range>,
+			bksge::indirect_strict_weak_order<Comp,
+				T const*,
+				bksge::projected<ranges::iterator_t<Range>, Proj>>
+		>::value>
+#endif
+	>
+	BKSGE_CXX14_CONSTEXPR ranges::borrowed_subrange_t<Range>
+	operator()(Range&& r, T const& value, Comp comp = {}, Proj proj = {}) const
+	{
+		return (*this)(
+			ranges::begin(r), ranges::end(r),
+			value, bksge::move(comp), bksge::move(proj));
+	}
+};
+
+inline namespace cpo
+{
+
+BKSGE_INLINE_VAR BKSGE_CONSTEXPR equal_range_fn equal_range{};
+
+}	// inline namespace cpo
+
+}	// namespace ranges
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_ALGORITHM_RANGES_EQUAL_RANGE_HPP
