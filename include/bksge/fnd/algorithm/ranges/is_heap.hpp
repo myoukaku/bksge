1fb04b5659db109eb67ffce0d14148010dec0843
myoukaku <myoukaku@hotmail.com>
2020-08-28 11:51:22 +0900

    Add ranges::is_heap

---
+++ include/bksge/fnd/algorithm/ranges/is_heap.hpp
@@ -0,0 +1,106 @@
+﻿/**
+ *	@file	is_heap.hpp
+ *
+ *	@brief	ranges::is_heap の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_ALGORITHM_RANGES_IS_HEAP_HPP
+#define BKSGE_FND_ALGORITHM_RANGES_IS_HEAP_HPP
+
+#include <bksge/fnd/algorithm/ranges/is_heap_until.hpp>
+#include <bksge/fnd/functional/ranges/less.hpp>
+#include <bksge/fnd/functional/identity.hpp>
+#include <bksge/fnd/iterator/concepts/random_access_iterator.hpp>
+#include <bksge/fnd/iterator/concepts/sentinel_for.hpp>
+#include <bksge/fnd/iterator/concepts/indirect_strict_weak_order.hpp>
+#include <bksge/fnd/iterator/projected.hpp>
+#include <bksge/fnd/ranges/concepts/random_access_range.hpp>
+#include <bksge/fnd/ranges/iterator_t.hpp>
+#include <bksge/fnd/ranges/begin.hpp>
+#include <bksge/fnd/ranges/end.hpp>
+#include <bksge/fnd/type_traits/enable_if.hpp>
+#include <bksge/fnd/type_traits/conjunction.hpp>
+#include <bksge/fnd/utility/move.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+namespace ranges
+{
+
+struct is_heap_fn
+{
+	template <
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+		bksge::random_access_iterator Iter,
+		bksge::sentinel_for<Iter> Sent,
+		typename Proj = bksge::identity,
+		bksge::indirect_strict_weak_order<
+			bksge::projected<Iter, Proj>
+		> Comp = ranges::less
+#else
+		typename Iter,
+		typename Sent,
+		typename Proj = bksge::identity,
+		typename Comp = ranges::less,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			bksge::random_access_iterator<Iter>,
+			bksge::sentinel_for<Sent, Iter>,
+			bksge::indirect_strict_weak_order<Comp,
+				bksge::projected<Iter, Proj>
+			>
+		>::value>
+#endif
+	>
+	BKSGE_CXX14_CONSTEXPR bool operator()(
+		Iter first, Sent last, Comp comp = {}, Proj proj = {}) const
+	{
+		return last == ranges::is_heap_until(
+			first, last,
+			bksge::move(comp),
+			bksge::move(proj));
+	}
+
+	template <
+#if defined(BKSGE_HAS_CXX20_CONCEPTS)
+		ranges::random_access_range Range,
+		typename Proj = bksge::identity,
+		bksge::indirect_strict_weak_order<
+			bksge::projected<ranges::iterator_t<Range>, Proj>
+		> Comp = ranges::less
+#else
+		typename Range,
+		typename Proj = bksge::identity,
+		typename Comp = ranges::less,
+		typename = bksge::enable_if_t<bksge::conjunction<
+			ranges::random_access_range<Range>,
+			bksge::indirect_strict_weak_order<Comp,
+				bksge::projected<ranges::iterator_t<Range>, Proj>
+			>
+		>::value>
+#endif
+	>
+	BKSGE_CXX14_CONSTEXPR bool
+	operator()(Range&& r, Comp comp = {}, Proj proj = {}) const
+	{
+		return (*this)(
+			ranges::begin(r), ranges::end(r),
+			bksge::move(comp), bksge::move(proj));
+	}
+};
+
+inline namespace cpo
+{
+
+BKSGE_INLINE_VAR BKSGE_CONSTEXPR is_heap_fn is_heap{};
+
+}	// inline namespace cpo
+
+}	// namespace ranges
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_ALGORITHM_RANGES_IS_HEAP_HPP
---
+++ test/src/fnd/algorithm/ranges/unit_test_fnd_algorithm_ranges_is_heap.cpp
@@ -0,0 +1,94 @@
+﻿/**
+ *	@file	unit_test_fnd_algorithm_ranges_is_heap.cpp
+ *
+ *	@brief	ranges::is_heap のテスト
+ *
+ *	@author	myoukaku
+ */
+
+#include <bksge/fnd/algorithm/ranges/is_heap.hpp>
+#include <bksge/fnd/algorithm/ranges/equal.hpp>
+#include <bksge/fnd/iterator/ranges/next.hpp>
+#include <bksge/fnd/functional/ranges/greater.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "constexpr_test.hpp"
+#include "ranges_test.hpp"
+
+namespace bksge_algorithm_test
+{
+
+namespace ranges_is_heap_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+inline BKSGE_CXX14_CONSTEXPR bool test01()
+{
+	namespace ranges = bksge::ranges;
+	{
+		int a[] { 1, 2, 3 };
+		VERIFY(false == ranges::is_heap(a, a + 3));
+	}
+	{
+		int a[] { 1, 2, 3 };
+		VERIFY(true  == ranges::is_heap(a, a + 3, ranges::greater{}));
+	}
+	{
+		int a[] { 3, 2, 1 };
+		VERIFY(true  == ranges::is_heap(a));
+	}
+	{
+		int a[] { 3, 2, 1 };
+		VERIFY(false == ranges::is_heap(a, ranges::greater{}));
+	}
+	{
+		int a[] { 9, 5, 4, 1, 1, 3, 2, 6 };
+		test_range<int, random_access_iterator_wrapper> r(a);
+		VERIFY(false == ranges::is_heap(r));
+	}
+	{
+		int a[] { 9, 9, 7, 3, 4, 3, 2 };
+		test_range<int, random_access_iterator_wrapper> r(a);
+		VERIFY(true  == ranges::is_heap(r));
+	}
+	return true;
+}
+
+inline bool test02()
+{
+	namespace ranges = bksge::ranges;
+	{
+		std::vector<int> a { 9, 9, 7, 3, 4, 3, 2 };
+		VERIFY(true  == ranges::is_heap(a));
+	}
+	{
+		std::vector<int> a { 9, 7, 7, 8, 4, 3, 2 };
+		VERIFY(false == ranges::is_heap(a));
+	}
+	{
+		std::vector<int> a { 3, 2, 1 };
+		VERIFY(true  == ranges::is_heap(a, [](int x, int y){return x < y;}));
+	}
+	{
+		std::vector<int> a { 1, 2, 3 };
+		VERIFY(false == ranges::is_heap(a, [](int x, int y){return x < y;}));
+	}
+	{
+		std::vector<int> a { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+		VERIFY(true  == ranges::is_heap(a, [](int x, int y){return x < y;}));
+	}
+	return true;
+}
+
+#undef VERIFY
+
+GTEST_TEST(AlgorithmTest, RangesIsHeapTest)
+{
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test01());
+	EXPECT_TRUE(test02());
+}
+
+}	// namespace ranges_is_heap_test
+
+}	// namespace bksge_algorithm_test
