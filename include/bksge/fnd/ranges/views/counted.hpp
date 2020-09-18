f4c4351b42ad69e43a809ab5650b91c6477eb7b9
myoukaku <myoukaku@hotmail.com>
2020-09-18 16:38:47 +0900

    Add views::counted

---
+++ include/bksge/fnd/ranges/views/counted.hpp
@@ -0,0 +1,105 @@
+﻿/**
+ *	@file	counted.hpp
+ *
+ *	@brief	counted の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_RANGES_VIEWS_COUNTED_HPP
+#define BKSGE_FND_RANGES_VIEWS_COUNTED_HPP
+
+#include <bksge/fnd/ranges/views/subrange.hpp>
+#include <bksge/fnd/concepts/detail/require.hpp>
+#include <bksge/fnd/concepts/detail/overload_priority.hpp>
+#include <bksge/fnd/iterator/concepts/input_or_output_iterator.hpp>
+#include <bksge/fnd/iterator/concepts/random_access_iterator.hpp>
+#include <bksge/fnd/iterator/iter_difference_t.hpp>
+#include <bksge/fnd/iterator/counted_iterator.hpp>
+#include <bksge/fnd/iterator/default_sentinel.hpp>
+#include <bksge/fnd/utility/move.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+namespace ranges
+{
+
+namespace views
+{
+
+namespace detail
+{
+
+struct counted_fn
+{
+#if defined(BKSGE_HAS_CXX17_DEDUCTION_GUIDES) &&	\
+	defined(BKSGE_HAS_CXX17_IF_CONSTEXPR)     &&	\
+	defined(BKSGE_HAS_CXX14_RETURN_TYPE_DEDUCTION)
+
+	template <BKSGE_REQUIRES_PARAM(bksge::input_or_output_iterator, Iter)>
+	BKSGE_CONSTEXPR auto
+	operator()(Iter i, bksge::iter_difference_t<Iter> n) const
+	{
+		if constexpr (bksge::is_random_access_iterator<Iter>::value)
+		{
+			return ranges::subrange{i, i + n};
+		}
+		else
+		{
+			return ranges::subrange{
+				bksge::counted_iterator{bksge::move(i), n},
+				bksge::default_sentinel
+			};
+		}
+	}
+
+#else
+
+private:
+	template <BKSGE_REQUIRES_PARAM(bksge::random_access_iterator, Iter), typename Difference>
+	static BKSGE_CONSTEXPR auto
+	impl(Iter i, Difference n, bksge::detail::overload_priority<1>)
+	-> ranges::subrange<Iter, Iter>
+	{
+		return {i, i + n};
+	}
+
+	template <typename Iter, typename Difference>
+	static BKSGE_CONSTEXPR auto
+	impl(Iter i, Difference n, bksge::detail::overload_priority<0>)
+	-> ranges::subrange<
+		bksge::counted_iterator<Iter>,
+		bksge::default_sentinel_t>
+	{
+		return {
+			bksge::counted_iterator<Iter>{bksge::move(i), n},
+			bksge::default_sentinel
+		};
+	}
+
+public:
+	template <BKSGE_REQUIRES_PARAM(bksge::input_or_output_iterator, Iter)>
+	BKSGE_CONSTEXPR auto
+	operator()(Iter i, bksge::iter_difference_t<Iter> n) const
+	->decltype(impl(bksge::move(i), n, bksge::detail::overload_priority<1>{}))
+	{
+		return impl(bksge::move(i), n, bksge::detail::overload_priority<1>{});
+	}
+
+#endif
+};
+
+}	// namespace detail
+
+BKSGE_INLINE_VAR BKSGE_CONSTEXPR
+views::detail::counted_fn counted{};
+
+}	// namespace views
+
+}	// namespace ranges
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_RANGES_VIEWS_COUNTED_HPP
---
+++ test/src/fnd/ranges/views/unit_test_fnd_ranges_counted.cpp
@@ -0,0 +1,78 @@
+﻿/**
+ *	@file	unit_test_fnd_ranges_counted.cpp
+ *
+ *	@brief	ranges::counted のテスト
+ *
+ *	@author	myoukaku
+ */
+
+#include <bksge/fnd/ranges/views/counted.hpp>
+#include <bksge/fnd/ranges/concepts/view.hpp>
+#include <bksge/fnd/ranges/concepts/sized_range.hpp>
+#include <bksge/fnd/ranges/concepts/common_range.hpp>
+#include <bksge/fnd/ranges/concepts/random_access_range.hpp>
+#include <bksge/fnd/algorithm/ranges/equal.hpp>
+#include <gtest/gtest.h>
+#include "constexpr_test.hpp"
+#include "ranges_test.hpp"
+
+namespace bksge_ranges_test
+{
+
+namespace counted_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+inline BKSGE_CXX14_CONSTEXPR bool test01()
+{
+	namespace ranges = bksge::ranges;
+	namespace views  = ranges::views;
+
+	int x[] = { 0,1,2,3,4,5,6,7,8,9 };
+	auto v = views::counted(x, 5);
+
+	int y[] = { 0,1,2,3,4 };
+	VERIFY(ranges::equal(v, y));
+
+	using R = decltype(v);
+	static_assert( ranges::is_view<R>::value, "");
+	static_assert( ranges::is_sized_range<R>::value, "");
+	static_assert( ranges::is_common_range<R>::value, "");
+	static_assert( ranges::is_random_access_range<R>::value, "");
+
+	return true;
+}
+
+inline BKSGE_CXX14_CONSTEXPR bool test02()
+{
+	namespace ranges = bksge::ranges;
+	namespace views  = ranges::views;
+
+	int x[] = { 0,1,2,3,4,5,6,7,8,9 };
+	test_forward_range<int> rx(x);
+	auto v = views::counted(rx.begin(), 4);
+
+	int y[] = { 0,1,2,3 };
+	VERIFY(ranges::equal(v, y));
+
+	using R = decltype(v);
+	static_assert( ranges::is_view<R>::value, "");
+	static_assert( ranges::is_sized_range<R>::value, "");
+	static_assert(!ranges::is_common_range<R>::value, "");
+	static_assert(!ranges::is_random_access_range<R>::value, "");
+
+	return true;
+}
+
+#undef VERIFY
+
+GTEST_TEST(RangesTest, CountedTest)
+{
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test01());
+	BKSGE_CXX14_CONSTEXPR_EXPECT_TRUE(test02());
+}
+
+}	// namespace counted_test
+
+}	// namespace bksge_ranges_test
