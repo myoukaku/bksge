27c97bbecbfac39f94858db442c978d78b419823
myoukaku <myoukaku@hotmail.com>
2021-01-25 01:45:55 +0900

    Optimize ranges::copy

---
+++ include/bksge/fnd/type_traits/detail/is_memcpyable.hpp
@@ -0,0 +1,39 @@
+﻿/**
+ *	@file	is_memcpyable.hpp
+ *
+ *	@brief	is_memcpyable の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_TYPE_TRAITS_DETAIL_IS_MEMCPYABLE_HPP
+#define BKSGE_FND_TYPE_TRAITS_DETAIL_IS_MEMCPYABLE_HPP
+
+#include <bksge/fnd/type_traits/bool_constant.hpp>
+#include <bksge/fnd/type_traits/detail/is_nonvolatile_trivially_copyable.hpp>
+
+namespace bksge
+{
+
+namespace detail
+{
+
+// Whether two iterator types can be used with memcpy/memmove.
+template <typename OutputIter, typename InputIter>
+struct is_memcpyable : public bksge::false_type {};
+
+template <typename T>
+struct is_memcpyable<T*, T*>
+	: public detail::is_nonvolatile_trivially_copyable<T>
+{};
+
+template <typename T>
+struct is_memcpyable<T*, const T*>
+	: public detail::is_nonvolatile_trivially_copyable<T>
+{};
+
+}	// namespace detail
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_TYPE_TRAITS_DETAIL_IS_MEMCPYABLE_HPP
---
+++ include/bksge/fnd/type_traits/detail/is_nonvolatile_trivially_copyable.hpp
@@ -0,0 +1,39 @@
+﻿/**
+ *	@file	is_nonvolatile_trivially_copyable.hpp
+ *
+ *	@brief	is_nonvolatile_trivially_copyable の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_TYPE_TRAITS_DETAIL_IS_NONVOLATILE_TRIVIALLY_COPYABLE_HPP
+#define BKSGE_FND_TYPE_TRAITS_DETAIL_IS_NONVOLATILE_TRIVIALLY_COPYABLE_HPP
+
+#include <bksge/fnd/type_traits/conjunction.hpp>
+#include <bksge/fnd/type_traits/negation.hpp>
+#include <bksge/fnd/type_traits/is_volatile.hpp>
+#include <bksge/fnd/type_traits/is_trivially_copyable.hpp>
+
+namespace bksge
+{
+
+namespace detail
+{
+
+// Cannot use memcpy/memmove/memcmp on volatile types even if they are
+// trivially copyable, so ensure is_memcpyable<volatile int*, volatile int*>
+// and similar will be false.
+
+template <typename T>
+struct is_nonvolatile_trivially_copyable
+	: public bksge::conjunction<
+		bksge::negation<bksge::is_volatile<T>>,
+		bksge::is_trivially_copyable<T>
+	>
+{};
+
+}	// namespace detail
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_TYPE_TRAITS_DETAIL_IS_NONVOLATILE_TRIVIALLY_COPYABLE_HPP
