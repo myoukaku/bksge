462181f706eea060e145212ace13ba8a3cfab221
myoukaku <myoukaku@hotmail.com>
2021-02-05 23:49:10 +0900

    [cmath]Add ellint_1

---
+++ include/bksge/fnd/cmath/detail/ellint_1_impl.hpp
@@ -0,0 +1,113 @@
+﻿/**
+ *	@file	ellint_1_impl.hpp
+ *
+ *	@brief	ellint_1 関数の実装
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_CMATH_DETAIL_ELLINT_1_IMPL_HPP
+#define BKSGE_FND_CMATH_DETAIL_ELLINT_1_IMPL_HPP
+
+#include <bksge/fnd/cmath/isnan.hpp>
+#include <bksge/fnd/cmath/abs.hpp>
+#include <bksge/fnd/cmath/floor.hpp>
+#include <bksge/fnd/cmath/sin.hpp>
+#include <bksge/fnd/cmath/cos.hpp>
+#include <bksge/fnd/cmath/comp_ellint_1.hpp>
+#include <bksge/fnd/cmath/detail/ellint_rf.hpp>
+//#include <bksge/fnd/cmath/detail/ellint_rj.hpp>
+#include <bksge/fnd/numbers.hpp>
+#include <bksge/fnd/limits.hpp>
+#include <bksge/fnd/config.hpp>
+#include <cmath>
+
+namespace bksge
+{
+
+namespace detail
+{
+
+#if defined(__cpp_lib_math_special_functions) && (__cpp_lib_math_special_functions >= 201603)
+
+inline /*BKSGE_CONSTEXPR*/ double
+ellint_1_unchecked(double k, double phi_)
+{
+	return std::ellint_1(k, phi_);
+}
+
+inline /*BKSGE_CONSTEXPR*/ float
+ellint_1_unchecked(float k, float phi_)
+{
+	return std::ellint_1f(k, phi_);
+}
+
+inline /*BKSGE_CONSTEXPR*/ long double
+ellint_1_unchecked(long double k, long double phi_)
+{
+	return std::ellint_1l(k, phi_);
+}
+
+#else
+
+template <typename T>
+inline BKSGE_CXX14_CONSTEXPR T
+ellint_1_unchecked_2(T k, T phi_)
+{
+	//  Reduce phi_ to -pi/2 < phi_ < +pi/2.
+	const int n = static_cast<int>(bksge::floor(phi_ / bksge::pi_t<T>() + T(0.5)));
+	const T phi_red = phi_ - n * bksge::pi_t<T>();
+
+	const T s = bksge::sin(phi_red);
+	const T c = bksge::cos(phi_red);
+
+	const T F = s * bksge::detail::ellint_rf(c * c, T(1) - k * k * s * s, T(1));
+
+	if (n == 0)
+	{
+		return F;
+	}
+	else
+	{
+		return F + T(2) * n * bksge::comp_ellint_1(k);
+	}
+}
+
+template <typename T>
+inline BKSGE_CXX14_CONSTEXPR T
+ellint_1_unchecked(T k, T phi_)
+{
+	using value_type = bksge::float_promote_t<double, T>;
+	return static_cast<T>(ellint_1_unchecked_2(
+		static_cast<value_type>(k), static_cast<value_type>(phi_)));
+}
+
+#endif
+
+template <typename T>
+inline BKSGE_CXX14_CONSTEXPR T
+ellint_1_impl(T k, T phi_)
+{
+	if (bksge::isnan(k) || bksge::isnan(phi_))
+	{
+		return bksge::numeric_limits<T>::quiet_NaN();
+	}
+
+	if (bksge::abs(k) > T(1))
+	{
+		return bksge::numeric_limits<T>::quiet_NaN();
+	}
+
+	if (bksge::abs(k) == T(1) && bksge::abs(phi_) >= (bksge::pi_t<T>() / 2))
+	{
+		return bksge::numeric_limits<T>::infinity();
+	}
+
+	return ellint_1_unchecked(k, phi_);
+}
+
+}	// namespace detail
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_CMATH_DETAIL_ELLINT_1_IMPL_HPP
---
+++ include/bksge/fnd/cmath/ellint_1.hpp
@@ -0,0 +1,58 @@
+﻿/**
+ *	@file	ellint_1.hpp
+ *
+ *	@brief	ellint_1 関数の定義
+ *
+ *	@author	myoukaku
+ */
+
+#ifndef BKSGE_FND_CMATH_ELLINT_1_HPP
+#define BKSGE_FND_CMATH_ELLINT_1_HPP
+
+#include <bksge/fnd/cmath/detail/ellint_1_impl.hpp>
+#include <bksge/fnd/concepts/arithmetic.hpp>
+#include <bksge/fnd/concepts/detail/require.hpp>
+#include <bksge/fnd/type_traits/float_promote.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+/**
+ *	@brief	第一種不完全楕円積分 (incomplete elliptic integral of the first kind) を計算する。
+ *
+ *	@param	k
+ *	@param	phi_
+ *
+ *	@return	引数 k, phi_ の第一種不完全楕円積分を返す
+ *
+ *	引数がどれか1つでも NaN のとき、NaN を返す
+ *	|k| > 1 のとき、NaN を返す(std::ellint_1 と違い、例外を投げない)
+ *	|k| = 1 かつ |phi_| >= π / 2 のとき、inf を返す
+ */
+template <
+	BKSGE_REQUIRES_PARAM(bksge::arithmetic, Arithmetic1),
+	BKSGE_REQUIRES_PARAM(bksge::arithmetic, Arithmetic2)
+>
+inline BKSGE_CXX14_CONSTEXPR bksge::float_promote_t<Arithmetic1, Arithmetic2>
+ellint_1(Arithmetic1 k, Arithmetic2 phi_) BKSGE_NOEXCEPT
+{
+	using type = bksge::float_promote_t<Arithmetic1, Arithmetic2>;
+	return detail::ellint_1_impl(static_cast<type>(k), static_cast<type>(phi_));
+}
+
+inline BKSGE_CXX14_CONSTEXPR float
+ellint_1f(float k, float phi_) BKSGE_NOEXCEPT
+{
+	return detail::ellint_1_impl(k, phi_);
+}
+
+inline BKSGE_CXX14_CONSTEXPR long double
+ellint_1l(long double k, long double phi_) BKSGE_NOEXCEPT
+{
+	return detail::ellint_1_impl(k, phi_);
+}
+
+}	// namespace bksge
+
+#endif // BKSGE_FND_CMATH_ELLINT_1_HPP
