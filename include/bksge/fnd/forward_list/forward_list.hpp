1a1dcb96ca3f8445621c8a281f7f91f3a8480cce
myoukaku <myoukaku@hotmail.com>
2021-02-15 13:44:05 +0900

    [forward_list]Add erase, erase_if

--- include/bksge/fnd/forward_list/forward_list.hpp
+++ include/bksge/fnd/forward_list/forward_list.hpp
@@ -18,4 +18,49 @@ using std::forward_list;
 
 }	// namespace bksge
 
+#if defined(__cpp_lib_erase_if) && (__cpp_lib_erase_if >= 202002)
+
+namespace bksge
+{
+
+using std::erase;
+using std::erase_if;
+
+}	// namespace bksge
+
+#else	// __cpp_lib_erase_if
+
+#include <bksge/fnd/iterator/distance.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+template <typename T, typename Alloc, typename Predicate>
+inline BKSGE_CXX14_CONSTEXPR
+typename forward_list<T, Alloc>::size_type
+erase_if(forward_list<T, Alloc>& c, Predicate pred)
+{
+#if defined(__cpp_lib_list_remove_return_type) && (__cpp_lib_list_remove_return_type >= 201806)
+	return c.remove_if(pred);
+#else
+	auto const sz = bksge::distance(c.begin(), c.end());
+	c.remove_if(pred);
+	return sz - bksge::distance(c.begin(), c.end());
+#endif
+}
+
+template <typename T, typename Alloc, typename U>
+inline BKSGE_CXX14_CONSTEXPR
+typename forward_list<T, Alloc>::size_type
+erase(forward_list<T, Alloc>& c, U const& value)
+{
+	using elem_type = typename forward_list<T, Alloc>::value_type;
+	return bksge::erase_if(c, [&](elem_type& elem) { return elem == value; });
+}
+
+}	// namespace bksge
+
+#endif	// __cpp_lib_erase_if
+
 #endif // BKSGE_FND_FORWARD_LIST_FORWARD_LIST_HPP
---
+++ test/src/fnd/forward_list/unit_test_fnd_forward_list_erase_if.cpp
@@ -0,0 +1,51 @@
+﻿/**
+ *	@file	unit_test_fnd_forward_list_erase_if.cpp
+ *
+ *	@brief	erase_if のテスト
+ *
+ *	@author	myoukaku
+ */
+
+#include <bksge/fnd/forward_list.hpp>
+#include <bksge/fnd/algorithm/ranges/equal.hpp>
+#include <gtest/gtest.h>
+#include "constexpr_test.hpp"
+
+namespace bksge_forward_list_test
+{
+
+namespace erase_if_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+inline /*BKSGE_CXX14_CONSTEXPR*/ bool
+EraseIfTest()
+{
+	{
+		bksge::forward_list<int> v = {3,1,4,5,2};
+		auto r = bksge::erase_if(v, [](int x) { return x % 2 == 0; });
+		VERIFY(r == 2);
+		const int v2[] = {3,1,5,};
+		VERIFY(bksge::ranges::equal(v, v2));
+	}
+	{
+		bksge::forward_list<int> v = {3,1,4,1,5,9,2,6,5,3,5,};
+		auto r = bksge::erase_if(v, [](int x) { return x >= 5; });
+		VERIFY(r == 5);
+		const int v2[] = {3,1,4,1,2,3,};
+		VERIFY(bksge::ranges::equal(v, v2));
+	}
+	return true;
+}
+
+GTEST_TEST(ForwardListTest, EraseIfTest)
+{
+	/*BKSGE_CXX14_CONSTEXPR_*/EXPECT_TRUE(EraseIfTest());
+}
+
+#undef VERIFY
+
+}	// namespace erase_if_test
+
+}	// namespace bksge_forward_list_test
