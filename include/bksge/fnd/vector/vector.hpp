3aa3b232485df7c34fccfea6a617cedfbab1760e
myoukaku <myoukaku@hotmail.com>
2021-02-15 11:36:12 +0900

    [vector]Add erase, erase_if

--- include/bksge/fnd/vector/vector.hpp
+++ include/bksge/fnd/vector/vector.hpp
@@ -18,4 +18,47 @@ using std::vector;
 
 }	// namespace bksge
 
+#if defined(__cpp_lib_erase_if) && (__cpp_lib_erase_if >= 202002)
+
+namespace bksge
+{
+
+using std::erase;
+using std::erase_if;
+
+}	// namespace bksge
+
+#else	// __cpp_lib_erase_if
+
+#include <bksge/fnd/algorithm/remove.hpp>
+#include <bksge/fnd/algorithm/remove_if.hpp>
+#include <bksge/fnd/config.hpp>
+
+namespace bksge
+{
+
+template <typename T, typename Alloc, typename U>
+inline BKSGE_CXX14_CONSTEXPR
+typename vector<T, Alloc>::size_type
+erase(vector<T, Alloc>& c, U const& value)
+{
+	auto const sz = c.size();
+	c.erase(bksge::remove(c.begin(), c.end(), value), c.end());
+	return sz - c.size();
+}
+
+template <typename T, typename Alloc, typename Predicate>
+inline BKSGE_CXX14_CONSTEXPR
+typename vector<T, Alloc>::size_type
+erase_if(vector<T, Alloc>& c, Predicate pred)
+{
+	auto const sz = c.size();
+	c.erase(bksge::remove_if(c.begin(), c.end(), pred), c.end());
+	return sz - c.size();
+}
+
+}	// namespace bksge
+
+#endif	// __cpp_lib_erase_if
+
 #endif // BKSGE_FND_VECTOR_VECTOR_HPP
---
+++ test/src/fnd/vector/unit_test_fnd_vector_erase.cpp
@@ -0,0 +1,51 @@
+﻿/**
+ *	@file	unit_test_fnd_vector_erase.cpp
+ *
+ *	@brief	erase のテスト
+ *
+ *	@author	myoukaku
+ */
+
+#include <bksge/fnd/vector.hpp>
+#include <bksge/fnd/algorithm/ranges/equal.hpp>
+#include <gtest/gtest.h>
+#include "constexpr_test.hpp"
+
+namespace bksge_vector_test
+{
+
+namespace erase_test
+{
+
+#define VERIFY(...)	if (!(__VA_ARGS__)) { return false; }
+
+inline /*BKSGE_CXX14_CONSTEXPR*/ bool
+EraseTest()
+{
+	{
+		bksge::vector<int> v = {3,1,4,5,2};
+		auto r = bksge::erase(v, 1);
+		VERIFY(r == 1);
+		const int v2[] = {3,4,5,2,};
+		VERIFY(bksge::ranges::equal(v, v2));
+	}
+	{
+		bksge::vector<int> v = {9,1,9,9,2,3,9,9,9,4,5,9,9,9,9,};
+		auto r = bksge::erase(v, 9);
+		VERIFY(r == 10);
+		const int v2[] = {1,2,3,4,5};
+		VERIFY(bksge::ranges::equal(v, v2));
+	}
+	return true;
+}
+
+GTEST_TEST(VectorTest, EraseTest)
+{
+	/*BKSGE_CXX14_CONSTEXPR_*/EXPECT_TRUE(EraseTest());
+}
+
+#undef VERIFY
+
+}	// namespace erase_test
+
+}	// namespace bksge_vector_test
