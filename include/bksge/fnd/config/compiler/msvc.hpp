bc55b537e4468bc8e82535463547c215e871a6ed
myoukaku <myoukaku@hotmail.com>
2021-05-13 13:52:08 +0900

    Update msvc.hpp

--- include/bksge/fnd/config/compiler/msvc.hpp
+++ include/bksge/fnd/config/compiler/msvc.hpp
@@ -610,6 +610,10 @@
 #if (_MSC_FULL_VER >= 192829914)
 #endif
 
+// Visual Studio 2019 version 16.9.5
+#if (_MSC_FULL_VER >= 192829915)
+#endif
+
 #if (BKSGE_CXX_STANDARD >= 17)
 #  define BKSGE_HAS_CXX17_CONSTEXPR
 #endif
