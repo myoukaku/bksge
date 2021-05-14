a0c6f85e373e2bdb1e951d766251705df0929ffb
myoukaku <myoukaku@hotmail.com>
2021-05-14 16:16:21 +0900

    Remove Vulkan-Headers, Vulkan-Loader submodules

--- externals/vulkan-headers.cmake
+++
@@ -1,8 +0,0 @@
-﻿message("Configuring Vulkan-Headers")
-
-#add_library(vulkan-headers INTERFACE)
-target_include_directories(vulkan-headers
-  INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/externals/Vulkan-Headers/include
-)
-
-message("")
--- externals/vulkan-loader.cmake
+++
@@ -1,8 +0,0 @@
-﻿message("Configuring Vulkan-Loader")
-
-set(VulkanHeaders_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/externals/Vulkan-Headers/include)
-set(VulkanRegistry_DIR ${CMAKE_CURRENT_SOURCE_DIR}/externals/Vulkan-Headers/registry)
-option(BUILD_WSI_WAYLAND_SUPPORT "Build Wayland WSI support" OFF)
-add_subdirectory(externals/Vulkan-Loader)
-
-message("")
